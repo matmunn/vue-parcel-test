43fde9910741920a0d99589ef0fd99b6bdb98ffa
Mat Munn <mat@matmunn.me>
2019-06-27 13:30:05 +1000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <span>{{ count }}</span>
+  <button @click.prevent="increment">+</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import App from './App'
+
+var vm = new Vue({
+  el: '#root',
+  template: '<App />',
+  components: {
+    App,
+  },
+})
